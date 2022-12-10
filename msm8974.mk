ae0eb756ee4f4b432a9fc6f9aceb897a268b218a
nyyu <mail@nyyu.dev>
2022-12-10 20:21:52 +0100

    msm8974-common: Ship cgroups.json and task_profiles.json Google's prebuilt cgroups.json and task_profiles.json for products launched with previous API levels only covers ro.product.first_api_level >= 28. [1]
    Oneplus 5,5t were launched with Nougat (API level 25), so schedtune and
    task groups are completely broken. Since the system also checks
    /vendor/etc for vendor profiles, make a copy of cgroups_28.json
    and task_profiles_28.json and ship them to /vendor/etc. Profiles
    for previous API levels are all the same anyway.
    
    Test: boot and check /dev/stune/
    
    [1] system/core/libprocessgroup/profiles/
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Change-Id: If8c09fa1868f60a451ced9e094e5796e46545fc0

--- msm8974.mk
+++ msm8974.mk
@@ -139,6 +139,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1-service
 
+# libprocessgroup
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light-service.samsung
